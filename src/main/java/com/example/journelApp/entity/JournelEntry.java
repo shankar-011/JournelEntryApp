1a2d909179d6970d2a1dfcf8f3b83c0c4ab947e0
Shankar <shankarunni18@gmail.com>
2026-03-15 11:48:59 +0530

    Replace Lombok Fuction Data with Getter and Setter

--- src/main/java/com/example/journelApp/entity/JournelEntry.java
+++ src/main/java/com/example/journelApp/entity/JournelEntry.java
@@ -1,6 +1,7 @@
 package com.example.journelApp.entity;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,7 +9,8 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 
 @Document(collection="journal_entries")
-@Data
+@Getter
+@Setter
 public class JournelEntry {
     @Id
     private ObjectId id;
