44a339866ed924ab46d79fbf280cace501f90f13
Shankar <shankarunni18@gmail.com>
2026-04-02 16:16:17 +0530

    Added Logging

--- src/main/java/com/example/journelApp/service/UserService.java
+++ src/main/java/com/example/journelApp/service/UserService.java
@@ -3,27 +3,37 @@ package com.example.journelApp.service;
 import com.example.journelApp.entity.JournelEntry;
 import com.example.journelApp.entity.User;
 import com.example.journelApp.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-
+@Slf4j
 @Component
 public class UserService {
     @Autowired
     private UserRepository userRepository;
     private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-
+    @Transactional
     public void saveEntry(User user){
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setRoles(Arrays.asList("USER"));
-        userRepository.save(user);
+        try{
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            user.setRoles(Arrays.asList("USER"));
+            userRepository.save(user);
+        } catch (Exception e) {
+            log.trace("ha",e);
+            log.debug("ha",e);
+            log.info("ha");
+            log.warn("ha");
+            log.error("ha");
+        }
     }
     public void saveAdminEntry(User user){
         user.setPassword(passwordEncoder.encode(user.getPassword()));
